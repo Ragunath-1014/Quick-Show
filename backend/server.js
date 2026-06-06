6366f679ed4a6e424743e6dcb02bd310dba5842a
Ragunath-1014 <ragunath1014@gmail.com>
2026-06-06 19:43:00 +0530

    Modified backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const server = http.createServer(app);
 // SOCKET.IO
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://quickshow-03.vercel.app",
         credentials: true
     }
 });
@@ -64,7 +64,7 @@ io.on("connection", (socket) => {
 // MIDDLEWARE
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://quickshow-03.vercel.app",
         credentials: true
     })
 );
