36dc57f1190e2dc974853a5c018c97c28890b82c
Ragunath-1014 <ragunath1014@gmail.com>
2026-06-06 19:33:44 +0530

    Modified frontend/src/socket.js, frontend/src/api/axios.js and added new file frontend/vercel.json

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://quick-show-1ii5.onrender.com", {
     withCredentials: true,
+    transports: ["websocket"]
 });
 
 export default socket;
